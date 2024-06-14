694b398dd468411156e9726c6ea0b618cbeaf271
Дмитрий <fokin@MBP-Fokin.dlink>
2024-06-14 18:32:04 +0700

    Add search and filter buttons

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,25 @@
     <Header />
 
     <div class="p-10">
-      <div class="flex justify-between">
+      <div class="flex justify-between items-center">
         <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+
+        <div class="flex gap-4">
+          <select name="" class="py-2 px-3 rounded-md border outline-none" id="">
+            <option value="">По названию</option>
+            <option value="">По цене (сначала дешевые)</option>
+            <option value="">По цене (сначала дорогие)</option>
+          </select>
+
+          <div class="relative">
+            <img class="absolute top-3 left-4" src="/search.svg" alt="Search " />
+            <input
+              class="border rounded-md py-1.5 pl-11 pr-4 outline-none m-0 focus:border-gray-400"
+              type="text"
+              placeholder="Поиск..."
+            />
+          </div>
+        </div>
       </div>
 
       <CardList />
